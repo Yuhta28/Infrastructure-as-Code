5871d002214121f2deb12cfe0888215ca3c47726
Yutaro Mori <yuta_mori@outlook.jp>
2020-07-04 10:49:36 +0900

    fix ansible

--- terraform/create-ec2.tf
+++ terraform/create-ec2.tf
@@ -7,12 +7,6 @@ resource "aws_instance" "YutaInstance" {
   ami           = "ami-0a1c2ec61571737db"
   instance_type = "t2.micro"
 
-  #network_interface {
-  #  network_interface_id = "eni-01ca1ea8d5ace73c5"
-  #  device_index         = 0
-  #}
-
-  #security_groups = "sg-04d0175d02367f5ca"
   vpc_security_group_ids = ["sg-0b2cbd5fbd6f61d86"]
 
   tags = {
