99a012c4bc7d13e4b1f4146745cd30d745d106d8
Yutaro Mori <yuta_mori@outlook.jp>
2020-07-19 14:09:06 +0900

    add comment to create-ec2.tf

--- terraform/create-ec2.tf
+++ terraform/create-ec2.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   profile = "default" #AWS configureのデフォルト設定
-  region = "ap-northeast-1"
+  region = "ap-northeast-1" #東京リージョン
 }
 
 resource "aws_instance" "YutaInstance" {
-  ami           = "ami-06ad9296e6cf1e3cf"
+  ami           = "ami-06ad9296e6cf1e3cf" #Amazon Linux2
   instance_type = "t2.micro"
 
-  vpc_security_group_ids = ["sg-0b2cbd5fbd6f61d86"]
+  vpc_security_group_ids = ["sg-0b2cbd5fbd6f61d86"] #SSH,HTTP,ICMP allow
 
   tags = {
     Name = "YutaInstance-terraform"
